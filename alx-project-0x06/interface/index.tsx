8ca8fab9b35dc5a1d98d6a19439627c911ccdbb4
ahmed <ahmedabdelnaser3010@gmail.com>
2025-06-15 01:06:36 +0300

    task three

---
+++ alx-project-0x06/interface/index.tsx
@@ -0,0 +1,17 @@
+import { ReactNode } from "react"
+
+// interfaces/index.ts
+export interface ButtonProps {
+    buttonLabel: string
+    buttonSize?: string
+    buttonBackgroundColor?: 'red' | 'blue' | 'orange' | 'green'
+    action?: () => void
+}
+
+export interface LayoutProps {
+    children: ReactNode
+}
+
+export interface PageRouteProps {
+    pageRoute: string
+}
